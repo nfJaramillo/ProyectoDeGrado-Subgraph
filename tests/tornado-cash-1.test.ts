36556faed23431d7b2138d2a1e959e3d1c44444c
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-09-14 09:02:46 -0500

    Initial commit

---
+++ tests/tornado-cash-1.test.ts
@@ -0,0 +1,60 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Bytes, BigInt, Address } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { Deposit } from "../generated/TornadoCash1/TornadoCash1"
+import { handleDeposit } from "../src/tornado-cash-1"
+import { createDepositEvent } from "./tornado-cash-1-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let commitment = Bytes.fromI32(1234567890)
+    let leafIndex = BigInt.fromI32(234)
+    let timestamp = BigInt.fromI32(234)
+    let newDepositEvent = createDepositEvent(commitment, leafIndex, timestamp)
+    handleDeposit(newDepositEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "commitment",
+      "1234567890"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "leafIndex",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "timestamp",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
