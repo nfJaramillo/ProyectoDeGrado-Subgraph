d7310bb71ae7f40addee6612da9c36900fa28dbe
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-09-14 10:37:00 -0500

    First smart contract (Only deposits)

--- src/tornado-cash-1.ts
+++ src/tornado-cash-1.ts
@@ -4,28 +4,29 @@ import {
   Deposit,
   Withdrawal
 } from "../generated/TornadoCash1/TornadoCash1"
-import { ExampleEntity } from "../generated/schema"
+import { Deposits, Withdrawals } from "../generated/schema"
 
 export function handleDeposit(event: Deposit): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Deposits.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new Deposits(event.transaction.hash.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity.commitment = event.params.commitment
-  entity.leafIndex = event.params.leafIndex
+  entity.from = event.transaction.from
+  entity.to = event.transaction.to
+  entity.value = event.transaction.value
+  entity.gasLimit = event.transaction.gasLimit
+  entity.gasPrice = event.transaction.gasPrice
+  entity.timestamp = event.block.timestamp
+
 
   // Entities can be written to the store with `.save()`
   entity.save()
@@ -66,4 +67,33 @@ export function handleDeposit(event: Deposit): void {
   // - contract.nextIndex(...)
 }
 
-export function handleWithdrawal(event: Withdrawal): void {}
+export function handleWithdrawal(event: Withdrawal): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Withdrawals.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Withdrawals(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+    entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count.plus(BigInt.fromI32(1))
+
+  // Entity fields can be set based on event parameters
+  entity.nullifierHash = event.params.nullifierHash
+  entity.relayer = event.params.relayer
+  entity.fee = event.params.fee
+  entity.timestamp = event.block.timestamp
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+
+
+
+}
