e2ced97945f12c35688c0666f659b677023f3479
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2022-10-25 15:07:50 -0500

    Fix on withdrawals's id

--- src/tornado-cash-4.ts
+++ src/tornado-cash-4.ts
@@ -36,12 +36,12 @@ import { Deposit, Withdrawal } from "../generated/schema"
   export function handleWithdrawal(event: WithdrawalEvent): void {
     // Entities can be loaded from the store using a string ID; this ID
     // needs to be unique across all entities of the same type
-    let entity = Withdrawal.load(event.transaction.from.toHex())
+    let entity = Withdrawal.load(event.transaction.hash.toHex())
   
     // Entities only exist after they have been saved to the store;
     // `null` checks allow to create entities on demand
     if (!entity) {
-      entity = new Withdrawal(event.transaction.from.toHex())
+      entity = new Withdrawal(event.transaction.hash.toHex())
   
     }
   
